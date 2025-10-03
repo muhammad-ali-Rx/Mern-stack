e5d614cdd4f67959af3a12a8c0c2fda99f8a3afe
asp <asp@aptech.com>
2025-10-03 20:25:56 +0500

    Update project files

--- Controller/ProdcutController.mjs
+++ Controller/ProdcutController.mjs
@@ -32,6 +32,32 @@ export const createProduct = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+export const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
+export const deleteProduct = async (req, res) => {
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 
--- Routes/ProductRoute.mjs
+++ Routes/ProductRoute.mjs
@@ -2,7 +2,9 @@ import express from "express";
 import {
     getAllProducts,
     getProductById,
-    createProduct
+    createProduct,
+    updateProduct,
+    deleteProduct
 
 } from '../Controller/ProdcutController.mjs';
 
@@ -11,6 +13,8 @@ const router = express.Router();
 router.get('/products', getAllProducts);
 router.get('/:id', getProductById);
 router.post('/add/products', createProduct);
+router.put('/update/:id', updateProduct);
+router.delete('/delete/:id', deleteProduct);
 
 
 
